4b33150e71a6b2c748e40f6690ca43c6c0ccadcc
Alba Giraldo <agiraldomolina@gmail.com>
2023-06-15 14:11:36 -0400

    display countries names

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import data  from './data/countries/countries.js';
+import data from "./data/countries/countries.js";
 // import data from './data/rickandmorty/rickandmorty.js';
 
 const countries = data.countries;
@@ -6,9 +6,21 @@ const countries = data.countries;
 const commonCountriesNames = [];
 
 countries.forEach((country) => {
-  commonCountriesNames.push(country.name.common)
+  commonCountriesNames.push(country.name.common);
 });
 
 const sortedCommonCountriesNames = commonCountriesNames.slice().sort();
 
 console.log(sortedCommonCountriesNames);
+
+const containerCountriesList = document.querySelector(
+  ".common-countries-name-ul"
+);
+
+containerCountriesList.innerHTML = "";
+
+sortedCommonCountriesNames.forEach((country) => {
+  const html = `
+  <li>${country}</li>`;
+  containerCountriesList.insertAdjacentHTML("beforebegin", html);
+});
