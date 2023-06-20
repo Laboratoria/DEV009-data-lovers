b50da42771a8a014de4f3b0ed3ea6a179ae2cb32
Nancy Nallely Martinez Dominguez <nancymartinezdominguez11@gmail.com>
2023-06-19 20:43:55 -0600

    maquetado films

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/ghibli/ghibli.js';
 
 console.log(example, data);
+
+
+function paginaprincipal() {
+    location.href = "index.html";
+}
+
