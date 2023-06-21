b9ae5e95bb13bcd946d6af28599f3dbf60eb9cf1
Bernidelpino <b.delpinovillaseca@gmail.com>
2023-06-21 15:45:36 -0400

    prueba

--- src/main.js
+++ src/main.js
@@ -3,4 +3,12 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
+const button = document.getElementById("comenzar");
+button.addEventListener("click", function () {
+  window.location.href= 'filtrado.html';
+});
+
+
+
+
 console.log(example, data);
