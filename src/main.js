9daef48d7260cebf186ffbc21de82c36a9df8396
CAMIANAIS <cmamanicr@unsa.edu.pe>
2023-06-21 00:03:36 -0500

    html sin importacion data

--- src/main.js
+++ src/main.js
@@ -1,6 +1,32 @@
-import { example } from './data.js';
+//import { films } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+//import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/ghibli/ghibli.js'
 
-console.log(example, data);
+
+document.addEventListener("DOMContentLoaded", function() {
+    const tableBody = document.querySelector("#table-body");
+
+    // Loop a través de los datos y crea las filas en la tabla
+    data.forEach((item) => {
+      // Crea una nueva fila
+      const row = document.createElement("tr");
+
+      // Crea las celdas y asigna los valores
+      const titleCell = document.createElement("td");
+      titleCell.textContent = item.title;
+      row.appendChild(titleCell);
+
+      const descriptionCell = document.createElement("td");
+      descriptionCell.textContent = item.description;
+      row.appendChild(descriptionCell);
+
+      const directorCell = document.createElement("td");
+      directorCell.textContent = item.director;
+      row.appendChild(directorCell);
+
+      // Agrega la fila al tbody
+      tableBody.appendChild(row);
+    });
+});
