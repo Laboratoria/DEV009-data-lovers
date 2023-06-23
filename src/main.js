dcc04637a0b474136a0415aacf017d9682c4df0f
GracielaFaz <graciefaz@gmail.com>
2023-06-23 12:31:09 -0600

    Primer historia de usuario lista

--- src/main.js
+++ src/main.js
@@ -15,6 +15,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     films.forEach((film) => {
 
       const containerArticle = document.createElement('article');
+      containerArticle.classList.add('data-peliculas-contenedor');
       dataContainer.appendChild(containerArticle);
       
       const filmImg = document.createElement('img');
@@ -23,29 +24,32 @@ window.addEventListener('DOMContentLoaded', (event) => {
       containerArticle.appendChild(filmImg);
 
       const containerDiv = document.createElement('div');
+      containerDiv.classList.add('data-peliculas-texto');
       containerArticle.appendChild(containerDiv);
+    
 
       const filmTitle = document.createElement('p');
-      filmTitle.style.fontWeight = 'bold';
       filmTitle.textContent = film.title;
-      dataContainer.appendChild(filmTitle);
+      filmTitle.classList.add('data-title')
+      containerDiv.appendChild(filmTitle);
 
       const filmReleasedate = document.createElement('p');
-      filmReleasedate.style.fontWeight = 'bold';
       filmReleasedate.textContent = 'Año: '+ film.release_date;
-      dataContainer.appendChild(filmReleasedate);
+      filmReleasedate.classList.add('data-year')
+      containerDiv.appendChild(filmReleasedate);
 
       const filmDescription = document.createElement('p');
       filmDescription.textContent =film.description;
-      dataContainer.appendChild(filmDescription);
+      filmDescription.classList.add('data-description')
+      containerDiv.appendChild(filmDescription);
 
       const filmDirector = document.createElement('p');
       filmDirector.textContent = 'Director: '+ film.director;
-      dataContainer.appendChild(filmDirector);
+      containerDiv.appendChild(filmDirector);
 
       const filmPoster = document.createElement('p');
       filmPoster.textContent ='Score: '+ film.rt_score;
-      dataContainer.appendChild(filmPoster);
+      containerDiv.appendChild(filmPoster);
   });
 });
 /*
