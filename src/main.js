71fa2373afb1235c68495983d7def127986dab74
Fabiola Granados <faby.granados@gmail.com>
2023-06-23 10:20:41 -0700

    logre que main.js tuviera las funciones de las 2

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
-import { example } from './data.js';
+
+import data from './data.js';
+import peliculas from './data/ghibli/ghibli.js';// Se importa la data de studio ghibli
 
 
-console.log(example, data);
+const botonVolverPrincipal = document.getElementById("principal");// Se declara la variable botonVolverPrincipal, y se llama al elemento del DOM por su Id.
+botonVolverPrincipal.addEventListener("click", paginaPrincipal);// Se crea un evento al dar click se dirige a la funcion pagina principal
+
+function paginaPrincipal() {
+    location.href = "index.html";// Se agrega a la funcion principal la localizacion para direccionar 
+}
 
 window.addEventListener("load", mostrarPeliculas(peliculas.films));// Se agrega el evento escucha para que cuando se cargue la pagina films se muestren las cards
 function mostrarPeliculas(peliculas) {
@@ -31,3 +38,32 @@ function mostrarPeliculas(peliculas) {
     });
     carSeccion.innerHTML = cards;
 }
+
+import data from './data/ghibli/ghibli.js';
+// import data from './data/lol/lol.js';
+
+console.log(data.films);
+
+let peopleElement = document.getElementById('people');
+data.films[10].people.forEach(person => {
+  // Crear un nuevo elemento para contener el nombre del personaje
+  let personElement = document.createElement('div');
+  personElement.classList.add('card'); // Agregamos una clase para dar estilo
+
+  let imagePeopleElement = document.createElement('img');
+  imagePeopleElement.classList.add('imagen'); // Clase para el estilo de las imágenes
+  imagePeopleElement.setAttribute('src', person.img);
+  imagePeopleElement.setAttribute('alt', person.name);
+  imagePeopleElement.setAttribute('id', person.name + '-img');
+
+  // crear un título para el nombre del personaje
+  let nameHeading = document.createElement('h2');
+  nameHeading.textContent = person.name;
+  personElement.appendChild(nameHeading);
+
+  // agregar la tarjeta al elemento #people
+  peopleElement.appendChild(personElement); 
+  personElement.appendChild(imagePeopleElement);
+});
+
+
