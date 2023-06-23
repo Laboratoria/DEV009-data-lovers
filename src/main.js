1be03fe81c78d9b960e0318009c7014788592ab4
Fabiola Granados <faby.granados@gmail.com>
2023-06-23 09:05:47 -0700

    se modifico el html y los js para la funcionalidad y la interaccion de datos

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
-import { example } from './data.js';
+import data from './data/ghibli/ghibli.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+console.log(data.films);
+
+let peopleElement = document.getElementById('people');
+data.films[10].people.forEach(person => {
+  // Crear un nuevo elemento para contener el nombre del personaje
+  let personElement = document.createElement('div');
+  personElement.classList.add('card'); // Agregamos una clase para dar estilo
+
+  let imagePeopleElement = document.createElement('img');
+  imagePeopleElement.classList.add('imagen'); // Clase para el estilo de las imágenes
+  imagePeopleElement.setAttribute('src', person.img);
+  imagePeopleElement.setAttribute('alt', person.name);
+  imagePeopleElement.setAttribute('id', person.name + '-img');
+
+  // crear un título para el nombre del personaje
+  let nameHeading = document.createElement('h2');
+  nameHeading.textContent = person.name;
+  personElement.appendChild(nameHeading);
+
+  // agregar la tarjeta al elemento #people
+  peopleElement.appendChild(personElement); 
+  personElement.appendChild(imagePeopleElement);
+});
