a60e1fab1c854719e1e8e54ecdf860b0647d5aa8
Natali <natalic1008.nm@gmail.com>
2023-06-26 11:01:32 -0500

    Listado de nombres y continentes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,29 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/countries/countries.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+console.log(data.countries[0].area);
+//console.log(example, data);
+const root =document.getElementById('root');
+const allCountries = data.countries;
+//root.innerHTML=`<h1>${data.countries[0].name.common}</h1>`;
+//const capital=document.createElement('h2');
+//capital.innerText=data.countries[0].capital;
+//const continente=document.createElement('h2');
+//continente.innerText=data.countries[0].subregion;
+
+//root.appendChild(capital);
+//root.appendChild(continente);
+for (let i = 0; i< allCountries.length; i++){
+   const name = document.createElement ('h2');
+   name.innerHTML=data.countries[i].name.common;
+   root.appendChild(name);
+};
+
+for (let i = 0; i< allCountries.length; i++){
+const continente = document.createElement ('h3');
+continente.innerHTML=data.countries[i].subregion;
+root.appendChild(continente);
+console.log(continente[2]);
+}
