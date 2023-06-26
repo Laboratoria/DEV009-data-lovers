6b98e1ca1bcd418532d6251a711ab9d4d5e1f4b2
BrennDev <134910223+BrennDev@users.noreply.github.com>
2023-06-26 10:01:54 -0600

    Filtro de continente Americano en consol log

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
-import { example } from './data.js';
+import { contAmerica } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/countries/countries.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+//console.log(example, data);
+
+const root = document.getElementById('root');
+const allCountries = data.countries;
+//const contAmerica = allCountries.continents;
+
+console.log(contAmerica(allCountries))
+
+
+
+
+
