c09feabacee0ff190c7fadfadb8a0de48dcc71d3
Natali <natalic1008.nm@gmail.com>
2023-06-27 12:37:34 -0500

    Visualizacion paises, banderas y continente

--- src/data.js
+++ src/data.js
@@ -4,6 +4,9 @@ export const contAmerica = (countries) => {
   return countries.filter((item) => item.continents[0] === 'America');
 };
 
+export const contCaribe = (countries) => {
+  return countries.filter((item) => item.continents[0] === 'caribe');
+};
 export const anotherExample = () => {
   return 'OMG';
 };
