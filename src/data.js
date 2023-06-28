9263b96e94e8422ca975688176594dd26d64fbf9
Nancy A Contreras <nancyalejandra24mv@gmail.com>
2023-06-27 21:06:33 -0500

    menu

--- src/data.js
+++ src/data.js
@@ -1,9 +1,12 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+import data from './data/pokemon/pokemon.js';
 
 export const anotherExample = () => {
   return 'OMG';
+};
+
+export const Muestra = () => {
+  const listExample = data.pokemon.slice().sort(() => 0.5 - Math.random()).slice(0, 5);
+  return listExample
 };
