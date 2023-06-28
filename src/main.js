864fb79b4e2cd4c0447a4a30d810d988f6a2e7be
BrennDev <134910223+BrennDev@users.noreply.github.com>
2023-06-27 19:07:21 -0600

    Tarjetas de banderas

--- src/main.js
+++ src/main.js
@@ -7,29 +7,18 @@ console.log(data.countries[0].area);
 //console.log(example, data);
 const root =document.getElementById('root');
 const allCountries = data.countries;
-//root.innerHTML=`<h1>${data.countries[0].name.common}</h1>`;
-//const capital=document.createElement('h2');
-//capital.innerText=data.countries[0].capital;
-//const continente=document.createElement('h2');
-//continente.innerText=data.countries[0].subregion;
-
-//root.appendChild(capital);
-//root.appendChild(continente);
+//Creacion de cards
 for (let i = 0; i< allCountries.length; i++){
-   const name = document.createElement ('h2');
-   const capital = document.createElement('h2');
-   const continente = document.createElement ('h3');
-   const imgFlags = document.createElement('img');
-   name.innerHTML=data.countries[i].name.common;
-   capital.innerHTML =data.countries[i].capital;
-   continente.innerHTML=data.countries[i].subregion;
-   imgFlags.src = data.countries[i].flags.png;
-   root.appendChild(name);
-   root.appendChild(capital);
-   root.appendChild(continente);
-   root.appendChild(imgFlags);
-    
+   let card = document.createElement ('div');
+   card.className= 'card';
+   card.innerHTML= ` 
+      <img src=${allCountries[i].flags.png}>
+      <h2>${allCountries[i].name.common}</h2>
+   `
+   root.appendChild(card);
 };
 
 
 console.log(contAmerica(allCountries))
+
+
