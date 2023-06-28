0dbeb495686897c262f0c4bacbc98b86df4b5424
Aivy Aguirre <aivy3094@gmail.com>
2023-06-27 22:48:46 -0500

    Subiendo Css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import { example } from './data.js';
+import { pokemon } from './data.js';
 
 import data from './data/pokemon/pokemon.js';
 
 
-console.log(example, data);
+//console.log(example, data);
+
+
+const root = document.getElementById('root');
+root.innerHTML=`<h1>${data.pokemon[0].name}</h1>`;
