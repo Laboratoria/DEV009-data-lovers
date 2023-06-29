17214844660e7e511c609b992dcca684ccc6cf5f
jariannat <jaripache01@gmail.com>
2023-06-29 11:48:28 -0500

    data real

--- src/data.js
+++ src/data.js
@@ -1,23 +1,29 @@
 // estas funciones son de ejemplo
-
-
-
-const sortNameAsc = (data) =>{
-  const newData = data
-  newData.sort((a, b) => {
-    const nameA = a.name.toUpperCase(); // ignore upper and lowercase
-    const nameB = b.name.toUpperCase(); // ignore upper and lowercase
-    if (nameA < nameB) {
+//manera ascedente
+export const sortNameasc = (arr) => {
+  arr.sort((a, b) => {
+    if (a.name < b.name ) {
       return -1;
     }
-    if (nameA > nameB) {
+    if (a.name > b.name) {
       return 1;
     }
-  
-    // names must be equal
     return 0;
   });
-  return newData;
-};
 
-export default sortNameAsc
+  return arr;
+};
+ //manera descendente
+ export const sortNameDes = (arr) => {
+  arr.sort((a, b) => {
+    if (a.name > b.name ) {
+      return -1;
+    }
+    if (a.name < b.name) {
+      return 1;
+    }
+    return 0;
+  });
+
+  return arr;
+};
