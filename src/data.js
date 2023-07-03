e8f5834467bad9cf12bf3ae1e0694fd6a9bee7cc
oolidu <arquitecturaod@gmail.com>
2023-07-03 15:04:37 -0400

    se muestra data en el navegador

--- src/data.js
+++ src/data.js
@@ -1,68 +1,39 @@
-// estas funciones son de ejemplo
+//import { people } from './main.js';
 
-export const example = () => {
-  return 'example';
-};
+//Estamos usando JavaScript modular. Export e Import son declaraciones.
+//El uso de export/import permite compartir elementos (variables, funciones, clases) entre diferentes archivos en un proyecto de JS
+//Solo se agrega la declaración export antes de la declaración de la variable
 
-export const anotherExample = () => {
-  return 'OMG';
-};
 
+
+
+
+/*people.sort();
+
+console.log(people);*/
+
+ 
+
+/*
 //Ordenar alfabéticamente
-export const orderByAlphabetical = (array, getter, order = 'asc')=> {
-  array.sort( (a, b) => {
+export const orderByAlphabetical = (array, getter, order = 'asc') => {
+  array.sort((a, b) => {
     const first = getter(a);
-    const second = getter (b);
+    const second = getter(b);
     //Compara los elementos
     const compare = first.localeCompare(second);
     //Devuelve la comparación en el orden que le pedimos
-    return order === 'asc' ? compare :-compare;
+    return order === 'asc' ? compare : -compare;
   })
   return array;
 }
-
-//Arreglo de people (aún no sé traerlo de la data así que pegué directamente el de una película)
-const people = [
-  {
-    "id": "fe93adf2-2f3a-4ec4-9f68-5422f1b87c01",
-    "name": "Pazu",
-    "img": "https://static.wikia.nocookie.net/studio-ghibli/images/8/8b/Pazu.jpg",
-    "gender": "Male",
-    "age": "13",
-    "eye_color": "Black",
-    "hair_color": "Brown",
-    "specie": "Human"
-  },
-  {
-    "id": "598f7048-74ff-41e0-92ef-87dc1ad980a9",
-    "name": "Lusheeta Toel Ul Laputa",
-    "img": "https://static.wikia.nocookie.net/studio-ghibli/images/c/c3/Sheeta.jpg",
-    "gender": "Female",
-    "age": "13",
-    "eye_color": "Black",
-    "hair_color": "Black",
-    "specie": "Human"
-  },
-  {
-    "id": "3bc0b41e-3569-4d20-ae73-2da329bf0786",
-    "name": "Dola",
-    "img": "https://static.wikia.nocookie.net/studio-ghibli/images/b/b3/Dola.png",
-    "gender": "Female",
-    "age": "60",
-    "eye_color": "Black",
-    "hair_color": "Peach",
-    "specie": "Human"
-  },
-]
-//Muestra el nuevo arreglo en la consola
-console.log(people);
 
 
 const sortedPeople = orderByAlphabetical(people, person => person.name);
 console.log(sortedPeople);
 
 
-const personList = document.getElementById('person-list');
+const peopleList = document.getElementById('people-list');
 
 
 sortedPeople.forEach(({ name, img, gender }) => {
@@ -72,5 +43,5 @@ sortedPeople.forEach(({ name, img, gender }) => {
     <p>Name: ${name}</p>
     <p>Gender: ${gender}</p>
   `;
-  personList.appendChild(li);
-});
+  peopleList.appendChild(li);
+});*/
--- src/main.js
+++ src/main.js
@@ -1,12 +1,37 @@
-import { orderByAlphabetical } from './data.js';
-
+//en este archivo se importar data y funciones desde diferentes archivos. También se hacen impresiones en la cónsola
+//se importa un objeto que llamaremos data desde el archivo './data/ghibli/ghibli.js' - el objeto tiene una exportación default
 import data from './data/ghibli/ghibli.js';
 
-// import { people } from './data/ghibli/ghibli.js';
 
-console.log(example, data.films);
-const ghibli = data.films;
+//Método map() es una función de los arreglos en JS que ITERA sobre cada elemento del arreglo y crea un nuevo arreglo
+const people = data.films.flatMap(film => film.people.map(person => ({
+  name: person.name,
+  image: person.img
+})));
+people.sort((a, b) => a.name.localeCompare(b.name));
+console.log(data, data.films, people);
 
-console.log(orderByAlphabetical (ghibli))
+//export { people }; aún no se sabe cómo usarlo efectivame en data.js
 
-// console.log(people);
+
+const peopleList = document.getElementById('people-list');
+peopleList.innerHTML = '';
+
+// Recorrer el arreglo ordenado y crear elementos HTML para cada persona
+people.forEach(person => {
+  const personItem = document.createElement('div');
+  const personImage = document.createElement('img');
+  const personName = document.createElement('p');
+
+  personImage.src = person.image;
+  personName.textContent = person.name;
+
+  personItem.appendChild(personImage);
+  personItem.appendChild(personName);
+
+  peopleList.appendChild(personItem);
+});
+
+
+
+
