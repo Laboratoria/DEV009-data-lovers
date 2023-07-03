9e5a9b372c9b08972e223f9bc780e974290a96c6
Paulina Torres <axs.pau@gmail.com>
2023-07-03 19:07:26 -0400

    terminamos funcion filter y no se si agregue algo XD

--- src/data.js
+++ src/data.js
@@ -8,8 +8,8 @@
 //export se utiliza para exportar esta función y hacerla accesible desde otros archivos o módulos.
 //data es un parámetro que representa los datos en los que se realizará la búsqueda
 //inputText es el texto de entrada que se utilizará para buscar en los datos.
-export const searchPokemon = (data, inputText) => {
-  const resultPokemon = [];
+/* export const searchPokemon = (data, inputText) => {
+  const resultPokemon = []; */
 
   //BUSCANDO LOS POKEMON CON EL TEXTO INGRESADO
 
@@ -17,15 +17,16 @@ export const searchPokemon = (data, inputText) => {
 
   //eachPokemon es el parámetro que representa cada elemento del array data. 
 
-  data.forEach((eachPokemon) => {
-    const namePokemon = eachPokemon.name;
-    const lengthText = inputText.length;
-    if (namePokemon.slice(0, lengthText) === inputText) {
-      resultPokemon.push(eachPokemon);
-    }
-  });
-  return resultPokemon;
-};
+//   data.forEach((eachPokemon) => {
+//     const namePokemon = eachPokemon.name;
+//     const lengthText = inputText.length;
+//     if (namePokemon.slice(0, lengthText) === inputText) {
+//       resultPokemon.push(eachPokemon);
+//     }
+//   });
+//   return resultPokemon;
+// };
+
 
 
 //fin nancy
