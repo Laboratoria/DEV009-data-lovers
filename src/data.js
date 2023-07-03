4392a253fe9641b82587e96d01045ffeb4881a50
Nancy A Contreras <nancyalejandra24mv@gmail.com>
2023-07-03 18:07:26 -0500

    se cambio en el filter el for por el metodo filter

--- src/data.js
+++ src/data.js
@@ -1,20 +1,24 @@
-
-
-
-
-
-
 // INICIO NANCY
 //export se utiliza para exportar esta función y hacerla accesible desde otros archivos o módulos.
 //data es un parámetro que representa los datos en los que se realizará la búsqueda
 //inputText es el texto de entrada que se utilizará para buscar en los datos.
 export const searchPokemon = (data, inputText) => {
-  const resultPokemon = [];
+  const resultPokemon = data.filter((eachPokemon) => {
+    const namePokemon = eachPokemon.name;
+    const lengthText = inputText.length;
+    return namePokemon.slice(0, lengthText) === inputText;
+  });
+  return resultPokemon;
+}
+
+
+
+
+  /// CON FOR 
+/*   const resultPokemon = [];
 
   //BUSCANDO LOS POKEMON CON EL TEXTO INGRESADO
-
   //forEach() es un método disponible en los arrays de JavaScript que permite ejecutar una función para cada elemento del array
-
   //eachPokemon es el parámetro que representa cada elemento del array data. 
 
   data.forEach((eachPokemon) => {
@@ -25,7 +29,10 @@ export const searchPokemon = (data, inputText) => {
     }
   });
   return resultPokemon;
-};
+}; */
+////
+
+
 
 
 //fin nancy
