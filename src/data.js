231642b951e80e1a9b7ef244183b1eb991f9a971
KellPit19 <kelly.g.pitter@gmail.com>
2023-07-04 15:02:53 -0400

    se creafunción display para primera historia

--- src/data.js
+++ src/data.js
@@ -1,9 +1,26 @@
 // estas funciones son de ejemplo
+export const display = (pokemonData)=>{
+  const seccionShowAll = document.getElementById('showAll');
+  const cardPokemon = document.createElement('div');
+  cardPokemon.classList.add(`pokemon-card`);
+  cardPokemon.classList.add(`${pokemonData.type[0]}`);
+  const namePokemon = document.createElement('p');
+  namePokemon.textContent = pokemonData.name;
+  const numPokemon = document.createElement('p');
+  numPokemon.textContent = pokemonData.num;
+  const imgPokemon = document.createElement('img');
+  imgPokemon.setAttribute('src',pokemonData.img);
+   
+  cardPokemon.appendChild(namePokemon);
+  cardPokemon.appendChild(imgPokemon);
+  cardPokemon.appendChild(numPokemon);
+  seccionShowAll.appendChild(cardPokemon);
 
-export const example = () => {
+};
+/*export const example = () => {
   return 'example';
 };
 
 export const anotherExample = () => {
   return 'OMG';
-};
+};*/
--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
-import { example } from './data.js';
+import { display } from './data.js';
+
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+const pokemonList = data.pokemon;
 
-console.log(example, data);
+
+pokemonList.forEach(function(pokemon){
+    display(pokemon);
+})
+
+
+
+
+
+
+
+console.log(pokemonList[0].type,pokemonList[0].name);
