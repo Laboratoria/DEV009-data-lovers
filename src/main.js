185d929c958e1b6f1be870fcbbae21f6ebeb8d22
Aivy Aguirre <aivy3094@gmail.com>
2023-07-07 01:40:39 -0500

    Pequeños cambios

--- src/main.js
+++ src/main.js
@@ -7,11 +7,11 @@ const container = document.getElementById("targets");
 
 //mostrar en el dom todos los pokemones
 const display = (pokemones) => {
-    container.innerHTML = ""
-    pokemones.forEach(element => {
-        const target = document.createElement('article')
-        target.classList.add('cards');
-        target.innerHTML += `<img src='${element.img}' alt='imagen de ${element.name}'>
+  container.innerHTML = ""
+  pokemones.forEach(element => {
+    const target = document.createElement('article')
+    target.classList.add('cards');
+    target.innerHTML += `<img src='${element.img}' alt='imagen de ${element.name}'>
         <h2>${element.name}</h2>
         <h4> Número: ${element.num}</h4>
         <h4>${element.generation.num}</h4>
@@ -19,8 +19,8 @@ const display = (pokemones) => {
         <h4> Tamaño: ${element.size.height}</h4>
         <h4> Peso: ${element.size.weight}</h4>
         <p>${element.about}</p>`
-        container.appendChild(target)
-    });
+    container.appendChild(target)
+  });
 }
 display(dataPokemon);
 
@@ -29,30 +29,26 @@ const menu = document.getElementById("menu");
 //const targets = document.querySelector(".cards");
 
 menu.addEventListener("change", () => {
-    const element = menu.options[menu.selectedIndex].value
-    if (element === "az") {
-        display(sortNameasc(dataPokemon))
-    }
-    else if (element === "za") {
-        display(sortNameDes(dataPokemon))
-    }
-    else if (element === "numberMayor") {
-        display(sortNumYouger(dataPokemon))
-    }
-    else if (element === "numberMenor") {
-        display(sortNumLower(dataPokemon))
-    } console.log(data);
+  const element = menu.options[menu.selectedIndex].value
+  if (element === "az") {
+    display(sortNameasc(dataPokemon))
+  }
+  else if (element === "za") {
+    display(sortNameDes(dataPokemon))
+  }
+  else if (element === "numberMayor") {
+    display(sortNumYouger(dataPokemon))
+  }
+  else if (element === "numberMenor") {
+    display(sortNumLower(dataPokemon))
+  } 
 })
-
 //filtro de buscar por nombre
- document.getElementById('pokeNames').addEventListener('keyup', ()=>{
+document.getElementById('pokeNames').addEventListener('keyup', () => {
 
-    const filterName = document.getElementById('pokeNames').value;
-    const result = dataPokemon.filter(element => (element.name).indexOf(filterName) !== -1);
-    display(result)
-    console.log("pokemones filtrados por nombre " , result)
-    
-      
+  const filterName = document.getElementById('pokeNames').value;
+  const result = dataPokemon.filter(element => (element.name).indexOf(filterName) !== -1);
+  display(result)
 })
 
 
