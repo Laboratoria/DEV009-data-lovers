c849cdf234bc0c9b66a8004c71fae7aff3740f4e
BrennDev <134910223+BrennDev@users.noreply.github.com>
2023-07-13 11:12:01 -0600

    Funcion orden de la A-Z, Test y funcion de filtrado por continente

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,4 +1,4 @@
-import { busqueda } from '../src/data.js';
+import { busqueda, orderAZ,orderZA } from '../src/data.js';
 const dataTestCountries = [{
   "name": {
     "common": "Colombia"
@@ -91,7 +91,7 @@ const dataTestCountries = [{
     "Oceania"
   ],
 }]
-
+/*Test busqueda*/
 describe('busqueda', () => {
   it('is a function', () => {
     expect(typeof busqueda).toBe('function');
@@ -118,8 +118,213 @@ describe('busqueda', () => {
     }]
     expect(busqueda(dataTestCountries,inputSearch)).toEqual(resultTestSearch);
   });
+});
 
-})
+/*Test ordenar A-Z*/
+describe('orderAZ', () => {
+  it('is a function', () => {
+    expect(typeof busqueda).toBe('function');
+  });
+
+  it('Ordenar de la A-Z', () => {
+    const resultTestOrderAZ = [{
+      "name": {
+        "common": "Algeria"
+      },
+      "capital": [
+        "Algiers"
+      ],
+      "subregion": "Northern Africa",
+      "languages": {
+        "ara": "Arabic"
+      },
+      "area": 2381741,
+      "population": 44700000,
+      "continents": [
+        "Africa"
+      ],
+    },  {
+      "name": {
+        "common": "Antarctica"
+      },
+      "area": 14000000,
+      "population": 1000,
+      "continents": [
+        "Antarctica"
+      ],
+    }, {
+      "name": {
+        "common": "Colombia"
+      },
+      "capital": [
+        "Bogotá"
+      ],
+      "subregion": "South America",
+      "languages": {
+        "spa": "Spanish"
+      },
+      "area": 1141748,
+      "population": 50882884,
+      "continents": [
+        "America"
+      ],
+    }, {
+      "name": {
+        "common": "Germany",
+      },
+      "capital": [
+        "Berlin"
+      ],
+      "subregion": "Western Europe",
+      "languages": {
+        "deu": "German"
+      },
+      "area": 357114,
+      "population": 83240525,
+      "continents": [
+        "Europe"
+      ],
+    }, {
+      "name": {
+        "common": "Micronesia"
+      },
+      "capital": [
+        "Palikir"
+      ],
+      "subregion": "Micronesia",
+      "languages": {
+        "eng": "English"
+      },
+      "area": 702,
+      "population": 115021,
+      "continents": [
+        "Oceania"
+      ],
+    }, {
+      "name": {
+        "common": "Uzbekistan"
+      },
+      "capital": [
+        "Tashkent"
+      ],
+      "subregion": "Central Asia",
+      "languages": {
+        "rus": "Russian",
+        "uzb": "Uzbek"
+      },
+      "area": 447400,
+      "population": 34232050,
+      "fifa": "UZB",
+      "continents": [
+        "Asia"
+      ],
+    } ]
+    expect(orderAZ(dataTestCountries)).toEqual(resultTestOrderAZ);
+  });
+});
+/*Test ordenar Z-A*/
+describe('orderZA', () => {
+  it('is a function', () => {
+    expect(typeof busqueda).toBe('function');
+  });
+
+  it('Ordenar de la Z-A', () => {
+    const resultTestOrderZA = [{
+      "name": {
+        "common": "Uzbekistan"
+      },
+      "capital": [
+        "Tashkent"
+      ],
+      "subregion": "Central Asia",
+      "languages": {
+        "rus": "Russian",
+        "uzb": "Uzbek"
+      },
+      "area": 447400,
+      "population": 34232050,
+      "fifa": "UZB",
+      "continents": [
+        "Asia"
+      ],
+    }, {
+      "name": {
+        "common": "Micronesia"
+      },
+      "capital": [
+        "Palikir"
+      ],
+      "subregion": "Micronesia",
+      "languages": {
+        "eng": "English"
+      },
+      "area": 702,
+      "population": 115021,
+      "continents": [
+        "Oceania"
+      ],
+    }, {
+      "name": {
+        "common": "Germany",
+      },
+      "capital": [
+        "Berlin"
+      ],
+      "subregion": "Western Europe",
+      "languages": {
+        "deu": "German"
+      },
+      "area": 357114,
+      "population": 83240525,
+      "continents": [
+        "Europe"
+      ],
+    }, {
+      "name": {
+        "common": "Colombia"
+      },
+      "capital": [
+        "Bogotá"
+      ],
+      "subregion": "South America",
+      "languages": {
+        "spa": "Spanish"
+      },
+      "area": 1141748,
+      "population": 50882884,
+      "continents": [
+        "America"
+      ],
+    }, {
+      "name": {
+        "common": "Antarctica"
+      },
+      "area": 14000000,
+      "population": 1000,
+      "continents": [
+        "Antarctica"
+      ],
+    }, {
+      "name": {
+        "common": "Algeria"
+      },
+      "capital": [
+        "Algiers"
+      ],
+      "subregion": "Northern Africa",
+      "languages": {
+        "ara": "Arabic"
+      },
+      "area": 2381741,
+      "population": 44700000,
+      "continents": [
+        "Africa"
+      ],
+    }]
+    expect(orderZA(dataTestCountries)).toEqual(resultTestOrderZA);
+  });
+});
+
 
 
  
