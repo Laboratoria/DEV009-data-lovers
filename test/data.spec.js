727c3e00687b2d3d5b653ee39bd04abd70120670
raudelgado <delgadoraulymar@gmail.com>
2023-07-17 10:35:17 -0500

    actualizacion en test, se asigno una sola constante

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,168 +1,56 @@
 import {filteredStatus, filteredSpecies, filteredGender, filteredOrder, filteredSearch, porcentajeCalculo} from '../src/data.js';
  
+const arrayTestData = [
+  {
+    "name": "Summer Smith",
+    "status": "Alive",
+    "species": "Human",
+    "gender": "Female",
+  },
+  {         
+    "name": "Amish Cyborg",
+    "status": "Dead",
+    "species": "Alien",
+    "gender": "Male",
+  },
+  {
+    "name": "Bepisian",
+    "status": "Alive",
+    "species": "Alien",
+    "gender": "unknown",
+  }];
 
 
 describe('test para filtrar status', () => {
-  it('test para validar status alive de los personjes', () => {
+  it('test para validar status alive de los personajes', () => {
     const statusAlive = 'Alive'
-    const dataStatusAlive = [{
-      "id": 1,
-      "name": "Rick Sanchez",
-      "status": "Alive",
-    },
-    {
-      "id": 2,
-      "name": "Morty Smith",
-      "status": "Alive",
-    },
-    {
-      "id": 3,
-      "name": "Summer Smith",
-      "status": "Alive",
-    },       {
-      "id": 7,
-      "name": "Abradolf Lincler",
-      "status": "unknown",
-    }]
-    const resultadoEsperadoAlive = [{
-      "id": 1,
-      "name": "Rick Sanchez",
-      "status": "Alive",
-    },
-    {
-      "id": 2,
-      "name": "Morty Smith",
-      "status": "Alive",
-    },
-    {
-      "id": 3,
-      "name": "Summer Smith",
-      "status": "Alive",
-    }]
-    expect(filteredStatus (dataStatusAlive, statusAlive)).toEqual(resultadoEsperadoAlive)
+    //const dataStatusAlive = arrayTestData 
+    const resultadoEsperadoAlive = [ 
+      {
+        "name": "Summer Smith",
+        "status": "Alive",
+        "species": "Human",
+        "gender": "Female",
+      },
+      {
+        "name": "Bepisian",
+        "status": "Alive",
+        "species": "Alien",
+        "gender": "unknown",
+      }];
+    expect(filteredStatus (arrayTestData, statusAlive)).toEqual(resultadoEsperadoAlive)
   });
   it('test para validar el status dead de los personajes', () => {
     const statusDead = 'Dead'
-    const dataStatusDead = [{
-      "id": 10,
-      "name": "Alan Rails",
-      "status": "Dead",
-      "species": "Human",
-      "type": "Superhuman (Ghost trains summoner)",
-      "gender": "Male",
-
-    },
-    {
-      "id": 11,
-      "name": "Albert Einstein",
-      "status": "Dead",
-      "species": "Human",
-      "type": "",
-      "gender": "Male",
-    },
-    {
-      "id": 12,
-      "name": "Alexander",
-      "status": "Dead",
-      "species": "Human",
-      "type": "",
-      "gender": "Male",
-    },      {
-      "id": 3,
-      "name": "Summer Smith",
-      "status": "Alive",
-      "species": "Human",
-      "type": "",
-      "gender": "Female",
-    }]
-    const resultadoEsperadoDead = [{
-      "id": 10,
-      "name": "Alan Rails",
-      "status": "Dead",
-      "species": "Human",
-      "type": "Superhuman (Ghost trains summoner)",
-      "gender": "Male",
-
-    },
-    {
-      "id": 11,
-      "name": "Albert Einstein",
-      "status": "Dead",
-      "species": "Human",
-      "type": "",
-      "gender": "Male",
-    },
-    {
-      "id": 12,
-      "name": "Alexander",
-      "status": "Dead",
-      "species": "Human",
-      "type": "",
-      "gender": "Male",
-    }]
-    expect(filteredStatus(dataStatusDead, statusDead)).toEqual(resultadoEsperadoDead)
-  });
-  it ('test para validar status unknown de los personajes', () => {
-    const statusUnknown = 'unknown'
-    const dataStatusUnknown = [
+    //const dataStatusDead = arrayTestData.status;
+    const resultadoEsperadoDead = [    
       {
-        "id": 13,
-        "name": "Alien Googah",
-        "status": "unknown",
-        "species": "Alien",
-        "type": "",
-        "gender": "unknown",
-      },
-      {
-        "id": 14,
-        "name": "Alien Morty",
-        "status": "unknown",
-        "species": "Alien",
-        "type": "",
-        "gender": "Male",
-      },
-      {
-        "id": 15,
-        "name": "Alien Rick",
-        "status": "unknown",
-        "species": "Alien",
-        "type": "",
-        "gender": "Male",
-      }, {
-        "id": 10,
-        "name": "Alan Rails",
+        "name": "Amish Cyborg",
         "status": "Dead",
-        "species": "Human",
-        "type": "Superhuman (Ghost trains summoner)",
+        "species": "Alien",
         "gender": "Male",
-  
-      },
-    ]
-    const resultadoEsperadoUnkwown = [{
-      "id": 13,
-      "name": "Alien Googah",
-      "status": "unknown",
-      "species": "Alien",
-      "type": "",
-      "gender": "unknown",
-    },
-    {
-      "id": 14,
-      "name": "Alien Morty",
-      "status": "unknown",
-      "species": "Alien",
-      "type": "",
-      "gender": "Male",
-    },
-    {
-      "id": 15,
-      "name": "Alien Rick",
-      "status": "unknown",
-      "species": "Alien",
-      "type": "",
-      "gender": "Male",
-    }] 
-    expect(filteredStatus(dataStatusUnknown, statusUnknown)).toEqual(resultadoEsperadoUnkwown)
+      }]
+    expect(filteredStatus(arrayTestData, statusDead)).toEqual(resultadoEsperadoDead)
   });
 });
 
@@ -171,98 +59,54 @@ describe('tests de filter species', () => {
   //test Alien
   it('test para validar que species es Alien', () => {
     const speciesOpcionPruebaAlien = 'Alien';
-    const datasetPruebaAlien = [{
-      "id": 470,
-      "name": "Alien Spa Employee",
-      "status": "Alive",
+    //const datasetPruebaAlien = [];
+    const resultadoEsperadoAlien = [{         
+      "name": "Amish Cyborg",
+      "status": "Dead",
       "species": "Alien",
-    }, 
-    {          
-      "id": 49,
-      "name": "Blamph",
-      "status": "Alive",
-      "species": "Alien",},
-    {
-      "id": 471,
-      "name": "Little Voltron",
-      "status": "Alive",
-      "species": "Robot",
-    }];
-    const resultadoEsperadoAlien = [{
-      "id": 470,
-      "name": "Alien Spa Employee",
-      "status": "Alive",
-      "species": "Alien",
-    }, 
-    {          
-      "id": 49,
-      "name": "Blamph",
-      "status": "Alive",
-      "species": "Alien",
-    }];
-    expect(filteredSpecies (datasetPruebaAlien, speciesOpcionPruebaAlien)).toEqual(resultadoEsperadoAlien);
-  });
-  //test Animal
-  it('test para validar que species es Animal', () => {
-    const speciesOpcionPruebaAnimal = 'Animal';
-    const datasetAnimal = [{
-      "id": 46,
-      "name": "Bill",
-      "status": "unknown",
-      "species": "Animal",
-      "type": "Dog",
-      "gender": "Male"},  
-    {
-      "id": 82,
-      "name": "Cronenberg Rick",
-      "status": "unknown",
-      "species": "Cronenberg",
-      "type": "",
       "gender": "Male",
-    }];
-    const resultadoEsperadoAnimal = [{
-      "id": 46,
-      "name": "Bill",
-      "status": "unknown",
-      "species": "Animal",
-      "type": "Dog",
-      "gender": "Male"
-    }];
-    expect(filteredSpecies(datasetAnimal, speciesOpcionPruebaAnimal)).toEqual(resultadoEsperadoAnimal);
-  });
-  // test Cronenberg
-  it('test para validar que species es Cronenberg', () => {
-    const speciesOpcionPruebaCronenberg = 'Cronenberg';
-    const datasetCronenberg = [{
-      "id": 82,
-      "name": "Cronenberg Rick",
-      "status": "unknown",
-      "species": "Cronenberg"},
-    {    
-      "id": 83,
-      "name": "Cronenberg Morty",
-      "status": "unknown",
-      "species": "Cronenberg",
     },
-    { 
-      "id": 46,
-      "name": "Bill",
-      "status": "unknown",
-      "species": "Animal",
-    }
-    ];
-    const resultadoEsperadoCronenberg = [{
-      "id": 82,
-      "name": "Cronenberg Rick",
-      "status": "unknown",
-      "species": "Cronenberg"},
-    {    
-      "id": 83,
-      "name": "Cronenberg Morty",
-      "status": "unknown",
-      "species": "Cronenberg",
+    {
+      "name": "Bepisian",
+      "status": "Alive",
+      "species": "Alien",
+      "gender": "unknown",
     }];
-    expect(filteredSpecies(datasetCronenberg, speciesOpcionPruebaCronenberg)).toEqual(resultadoEsperadoCronenberg);
+    expect(filteredSpecies (arrayTestData, speciesOpcionPruebaAlien)).toEqual(resultadoEsperadoAlien);
+  });
+  it('test para validar que species es Human', () => {
+    const speciesOpcionPruebaHuman = 'Human';
+    //const datasetCronenberg = ;
+    const resultadoEsperadoHuman = [  {
+      "name": "Summer Smith",
+      "status": "Alive",
+      "species": "Human",
+      "gender": "Female",
+    }];
+    expect(filteredSpecies(arrayTestData, speciesOpcionPruebaHuman)).toEqual(resultadoEsperadoHuman);
+  });
+  it('test para validar que se ha seleccionado All', () => {
+    const speciesOpcionAll = 'All';
+    //const datasetCronenberg = ;
+    const resultadoEsperadoAll = [   {
+      "name": "Summer Smith",
+      "status": "Alive",
+      "species": "Human",
+      "gender": "Female",
+    },
+    {         
+      "name": "Amish Cyborg",
+      "status": "Dead",
+      "species": "Alien",
+      "gender": "Male",
+    },
+    {
+      "name": "Bepisian",
+      "status": "Alive",
+      "species": "Alien",
+      "gender": "unknown",
+    }];
+    expect(filteredSpecies(arrayTestData, speciesOpcionAll)).toEqual(resultadoEsperadoAll);
   });
 });
 
@@ -270,138 +114,34 @@ describe('tests de filter species', () => {
 describe('tests de filter gender', () => {
   it('test para validar gender Male de los personajes', () => {
     const genderMale = 'Male'
-    const dataGenderMale = [{
-      "id": 1,
-      "name": "Rick Sanchez",
-      "status": "Alive",
-      "species": "Human",
-      "type": "",
+    // const dataGenderMale =  'Male'
+    const resultadoEsperadoMale = [{         
+      "name": "Amish Cyborg",
+      "status": "Dead",
+      "species": "Alien",
       "gender": "Male",
-    },
-    {
-      "id": 2,
-      "name": "Morty Smith",
-      "status": "Alive",
-      "species": "Human",
-      "type": "",
-      "gender": "Male",
-  
-    },
-    {
-      "id": 3,
-      "name": "Summer Smith",
-      "status": "Alive",
-      "species": "Human",
-      "type": "",
-      "gender": "Female",
- 
     }];
-    const resultadoEsperadoMale = [{
-      "id": 1,
-      "name": "Rick Sanchez",
-      "status": "Alive",
-      "species": "Human",
-      "type": "",
-      "gender": "Male",
-    },
-    {
-      "id": 2,
-      "name": "Morty Smith",
-      "status": "Alive",
-      "species": "Human",
-      "type": "",
-      "gender": "Male",
-  
-    }];
-    expect(filteredGender (dataGenderMale, genderMale)).toEqual(resultadoEsperadoMale);
+    expect(filteredGender (arrayTestData, genderMale)).toEqual(resultadoEsperadoMale);
   });
   it('test para validar gender Female de los personajes', () => { 
     const genderFemale = 'Female'
-    const dataGenderFemale = [{
-      "id": 3,
-      "name": "Summer Smith",
-      "status": "Alive",
-      "species": "Human",
-      "type": "",
-      "gender": "Female",
-    },
-    {
-      "id": 4,
-      "name": "Beth Smith",
-      "status": "Alive",
-      "species": "Human",
-      "type": "",
-      "gender": "Female",
-    },
-    {
-      "id": 5,
-      "name": "Jerry Smith",
-      "status": "Alive",
-      "species": "Human",
-      "type": "",
-      "gender": "Male",
-    }];
     const resultadoEsperadoFemale = [{
-      "id": 3,
       "name": "Summer Smith",
       "status": "Alive",
       "species": "Human",
-      "type": "",
-      "gender": "Female",
-    },
-    {
-      "id": 4,
-      "name": "Beth Smith",
-      "status": "Alive",
-      "species": "Human",
-      "type": "",
       "gender": "Female",
     }];
-    expect(filteredGender (dataGenderFemale, genderFemale)).toEqual(resultadoEsperadoFemale);
+    expect(filteredGender (arrayTestData, genderFemale)).toEqual(resultadoEsperadoFemale);
   }); 
   it('test para validar gender unkwown de los personajes', () => {
     const genderUnknown = 'unknown'
-    const dataGenderUnknown = [{
-      "id": 35,
-      "name": "Bepisian",
-      "status": "Alive",
-      "species": "Alien",
-      "type": "Bepisian",
-      "gender": "unknown",
-    },
-    {
-      "id": 36,
-      "name": "Beta-Seven",
-      "status": "Alive",
-      "species": "Alien",
-      "type": "Hivemind",
-      "gender": "unknown",
-    },
-    {
-      "id": 37,
-      "name": "Beth Sanchez",
-      "status": "Alive",
-      "species": "Human",
-      "type": "",
-      "gender": "Female",
-    }];
     const resultadoEsperadoUnknown = [{
-      "id": 35,
       "name": "Bepisian",
       "status": "Alive",
       "species": "Alien",
-      "type": "Bepisian",
-      "gender": "unknown",
-    },
-    {
-      "id": 36,
-      "name": "Beta-Seven",
-      "status": "Alive",
-      "species": "Alien",
-      "type": "Hivemind",
       "gender": "unknown",
     }];
-    expect(filteredGender (dataGenderUnknown, genderUnknown)).toEqual(resultadoEsperadoUnknown);
+    expect(filteredGender (arrayTestData, genderUnknown)).toEqual(resultadoEsperadoUnknown);
   });
 });
   
@@ -426,9 +166,35 @@ describe('test para filtrar por orden', () => {
       },
       {
         "name": "Secretary of the Interior",
-      }, 
-    ]
+      }
+    ];
     expect(filteredOrder(orderAsc, order)).toEqual(orderEsperado);
+  });
+  it('test para filtrar en orden descendente', () => {
+    const order = 'ZA'
+    const orderDesc = [
+      {
+        "name": "Abradolf Lincler",
+      },
+      {
+        "name": "Loggins",
+      },
+      {
+        "name": "Secretary of the Interior",
+      }];
+    const orderEsperadoDesc = [
+      {
+        "name": "Secretary of the Interior",
+      },
+      {
+        "name": "Loggins",
+      },
+      {
+        "name": "Abradolf Lincler",
+      },
+ 
+    ]
+    expect(filteredOrder(orderDesc, order)).toEqual(orderEsperadoDesc);
   });
 });
 
